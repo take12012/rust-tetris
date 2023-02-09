d26d1ae6298f635272cae4a7797b7ecd87f7153a
竹之内　瑞葉 <takenouchimizuha@takenouchizuihanoMacBook-Pro.local>
2023-02-09 21:26:42 +0900

    ミノのキー操作

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Rustで作るテトリス入門
-### 参考文献
+## 参考文献
 https://zenn.dev/kumavale/books/30efec2e1d3428
+
+
+## memo
+|  キー  |  説明  |
+| ---- | ---- |
+|  `#[derive(Clone, Copy)]`  |  Outer attribute。直後のアイテムに属性を付与する。  |
+|  &#124;=  |  ビットOR後に代入。オーバーロード「BitOrAssign」。  |
+|  getch-rs = "0.1"  |  外部クレート。キー入力の取得。`cargo add getch-rs`コマンドでもOK。  |
+|    |    |
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use std::{thread, time};
+use getch_rs::{Getch, Key};
 
-struct Position {
-    x: usize,
-    y: usize,
+// テトリミノの種類
+#[derive(Clone, Copy)]
+enum MinoKind {
+    I,
+    O,
+    S,
+    Z,
+    J,
+    L,
+    T
 }
 
-fn main() {
-
-    let minos = [
-       // Iミノ
-       [
+// テトリミノの形状
+const MINOS: [[[usize; 4]; 4]; 7] = [
+    // Iミノ
+    [
         [0,0,0,0],
         [0,0,0,0],
         [1,1,1,1],
@@ -57,7 +64,26 @@ fn main() {
         [1,1,1,0],
         [0,0,0,0],
     ],
-    ];
+];
+
+ // テトリミノがフィールドに衝突する場合は`ture`を返す
+ fn is_collision(field: &[[usize;12]], pos: &Position, mino: MinoKind) -> bool {
+    for y in 0..4 {
+        for x in 0..4 {
+            if field[y+pos.y][x+pos.x] & MINOS[mino as usize][y][x] == 1 {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+fn main() {
 
     let field = [
         [1,1,1,0,0,0,0,0,0,1,1,1],
@@ -86,22 +112,30 @@ fn main() {
     
     // 初期位置
     let mut pos = Position { x: 4, y: 0 };
-
+    let g = Getch::new();
+    
     // 画面クリア
     println!("\x1b[2J\x1b[H\x1b[?25l");
 
-    // 5マス分落下させてみる
-    for _ in 0..5 {
+    // 30マス分落下させてみる
+    loop {
         // 描画用フィールドの生成
         let mut field_buf = field;
+        // 自然落下
+        let new_pos = Position {
+            x: pos.x,
+            y: pos.y + 1,
+        };
+        if !is_collision(&field, &new_pos, MinoKind::I) {
+            // posの座標を更新
+            pos = new_pos;
+        }
         // 描画用フィールドにテトリミノの情報を書き込む
         for y in 0..4 {
             for x in 0..4 {
-                field_buf[y+pos.y+1][x+pos.x] |= minos[0][y][x];  // I
+                field_buf[y+pos.y][x+pos.x] |= MINOS[MinoKind::I as usize][y][x];
             }
         }
-        // posのy座標を更新
-        pos.y += 1;
         // フィールドを描画
         println!("\x1b[H");  // カーソルを先頭に移動
         for y in 0..22 {
@@ -117,6 +151,41 @@ fn main() {
 
         // 1秒間スリーブする
         thread::sleep(time::Duration::from_millis(1000));
+        // キー入力待ち
+        match g.getch() {
+            Ok(Key::Left) => {
+                let new_pos = Position {
+                    x: pos.x - 1,
+                    y: pos.y,
+                };
+                if !is_collision(&field, &new_pos, MinoKind::I) {
+                    // posの座標を更新
+                    pos = new_pos;
+                }
+            }
+            Ok(Key::Down) => {
+                let new_pos = Position {
+                    x: pos.x,
+                    y: pos.y + 1,
+                };
+                if !is_collision(&field, &new_pos, MinoKind::I) {
+                    // posの座標を更新
+                    pos = new_pos;
+                }
+            }
+            Ok(Key::Right) => {
+                let new_pos = Position {
+                    x: pos.x + 1,
+                    y: pos.y,
+                };
+                if !is_collision(&field, &new_pos, MinoKind::I) {
+                    // posの座標を更新
+                    pos = new_pos;
+                }
+            }
+            Ok(Key::Char('q')) => break,
+            _ => (),  // 何もしない
+       }
     }
 
     // カーソルを再表示
